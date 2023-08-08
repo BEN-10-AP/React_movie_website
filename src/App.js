f332dced41d48cc59b07656d6fe7cb5751d57ec0
Atul Pandey <79309767+BEN-10-AP@users.noreply.github.com>
2023-08-08 19:58:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Header from './components/Header';
+import Movies from './components/Movies';
+import movies from './movies.json'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    <div className='main'>
+      {
+        movies.map((element)=>{
+          return(<Movies 
+          title={element.Title}
+          year={element.Year}
+          image={element.Poster}
+          />
+          )
+        })
+      }
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <>
+    <h1 className="header">Movies</h1>
+    </>
+  )
+}
+
+export default Header
